a7f8a17237a18745aceb8de1c204a5392d4e4406
Marius van Eck <marius.v.eck@gmail.com>
2017-01-20 01:41:58 +0100

    Add functionality to Angular page

--- src/main/webapp/resources/static/js/entities.component.js
+++ src/main/webapp/resources/static/js/entities.component.js
@@ -5,10 +5,14 @@ angular.module('MusicDatabaseApp')
         templateUrl: 'resources/static/views/entities.template.html',
         controller: function EntityController(restController) {
             var self = this;
+            var entityTypes = ['artist', 'album', 'song'];
 
-            this.entityType = 'artist';
+            this.entityType = entityTypes[0];
+            this.parentType = '';
             this.nameField = '';
-            // this.parent = {artistId: ''};
+            this.parent = undefined;
+            this.filter = undefined;
+            this.showAdd = true;
 
             this.entities = [];
             this.newEntityName = "";
@@ -16,10 +20,12 @@ angular.module('MusicDatabaseApp')
             this.addEntity = function() {
                 var newEntity = {};
                 newEntity[self.nameField] = self.newEntityName;
+                if (this.parent) newEntity[self.parentType] = self.parent;
 
                 restController.addEntity(newEntity)
                     .then(function(response) {
                         self.newEntityName = "";
+                        if (!self.entities) self.entities = [];
                         self.entities.push(response);
                     });
             };
@@ -31,15 +37,32 @@ angular.module('MusicDatabaseApp')
                     });
             };
 
-            this.getEntities = function() {
+            this.getEntities = function(parent) {
 
                 var restUrl = 'http://localhost:8080/study-case/api/' + this.entityType + 's/';
                 restController.initEntities(restUrl).then(function(response) {
                     self.entities = response;
                     self.nameField = self.entityType + 'Name';
+                    self.parent = parent;
+                    if (parent) {
+                        self.parentType = entityTypes[entityTypes.indexOf(self.entityType) - 1];
+                        self.filter = {};
+                        self.filter[self.parentType] = {}
+                        self.filter[self.parentType][self.parentType + "Id"] = parent[self.parentType+"Id"];
+                        self.showAdd = true;
+                    } else {
+                        self.filter = undefined;
+                        self.showAdd = false
+                        if (self.entityType == 'artist') self.showAdd = true;
+                    }
                 })
             }
 
-            this.getEntities();
+            this.goInto = function(entity) {
+                self.entityType = entityTypes[entityTypes.indexOf(self.entityType) + 1];
+                self.getEntities(entity);
+            }
+
+            this.getEntities(undefined);
         }
     });
